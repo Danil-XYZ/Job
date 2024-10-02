e63f328aa92bb7e4a11704a7070becb76f31148d
Danil <voindan@gmail.com>
2024-10-02 23:55:39 +0300

    Final build

--- app/src/main/java/com/example/job/fragments/WelcomeFragment.kt
+++ app/src/main/java/com/example/job/fragments/WelcomeFragment.kt
@@ -42,6 +42,7 @@ class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
             }
         }
 
+
         welcomeEditText.addTextChangedListener {
             if (welcomeEditText.text.toString() != "") {
                 continueButton.isEnabled = true
